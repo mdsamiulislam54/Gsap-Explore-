7f0792da4676ee14a208a032b53470be64504210
mdsamiulislam54 <samiulm5332@gmail.com>
2025-05-12 11:35:26 +0600

    pin animation add

--- src/page/ScrollTriggerr.jsx
+++ src/page/ScrollTriggerr.jsx
@@ -11,6 +11,9 @@ const ScrollTriggerr = () => {
     const box2 = useRef()
     const box3 = useRef()
     const text =useRef()
+    const textContainer = useRef()
+
+  
 
 useEffect(()=>{
     gsap.from(box1.current,{
@@ -20,16 +23,9 @@ useEffect(()=>{
 
     })
     gsap.from(box2.current,{
-         scale:0,
-        duration:2,
-        delay:1,
-        scrollTrigger:{
-            trigger:box2.current,
-            markers: true,
-            start: "top 100%",
-             
-            
-        }
+        
+        
+       
     })
     gsap.from(box3.current,{
          scale:0,
@@ -37,23 +33,27 @@ useEffect(()=>{
         delay:1,
          scrollTrigger:{
             trigger:box3.current,
-        
+           
             scrub: 1, 
             
         }
     })
-    gsap.from(text.current,{
-        x:-500,
-        opacity:0,
-        duration:2,
-        scale:0,
-        delay:1,
+
+    let totalWidth = text.current.scrollWidth;
+    console.log(totalWidth)
+    gsap.to(text.current,{
+        x:()=>`-${totalWidth-window.innerWidth} px`,
+        ease: "none",
         scrollTrigger:{
-            trigger:text.current,
-            start: "top 100%", 
+            trigger: textContainer.current,
+            start:'top top',
+            end:()=>`+=${totalWidth}`,
             scroller:'body',
             markers:true,
-            scrub:1
+             scrub: 1, 
+             pin:true,
+             pinnedContainer:true
+             
         }
 
     })
@@ -63,11 +63,11 @@ useEffect(()=>{
         <div className='h-screen flex justify-center items-center bg-blue-300'>
             <div ref={box1} className=" w-50 h-50 bg-red-500 "></div>
         </div>
-        <div className='h-screen flex flex-col  justify-center items-center bg-blue-500 overflow-hidden'>
-                 <p ref={text} className='w-4/12 mx-auto text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta error repellat corrupti magnam ratione reiciendis, vero alias porro consequatur, at nesciunt facilis quasi! Quas, voluptatibus autem? Itaque in consectetur sequi.</p>
-            <div ref={box2} className=" w-50 h-50 bg-red-600 ">
+        <div ref={textContainer} className='h-screen flex flex-col  justify-center items-center bg-blue-500 overflow-x-hidden'>
+                 <p ref={text} className=' px-6  mx-auto text-center text-[40vw] whitespace-nowrap '>MD SHAMIUL ISLAM</p>
+            {/* <div ref={box2} className=" w-50 h-50 bg-red-600 ">
            
-            </div>
+            </div> */}
         </div>
         <div className='h-screen flex justify-center items-center bg-blue-900'>
             <div ref={box3} className=" w-50 h-50 bg-red-900 "></div>
