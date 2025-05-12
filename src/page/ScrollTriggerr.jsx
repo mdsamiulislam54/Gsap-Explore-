a879d6f71ad5265d8c336722141ceece31a8f6e3
mdsamiulislam54 <samiulm5332@gmail.com>
2025-05-12 10:06:32 +0600

    ScrollTrigger animaion add

--- src/page/ScrollTriggerr.jsx
+++ src/page/ScrollTriggerr.jsx
@@ -10,6 +10,7 @@ const ScrollTriggerr = () => {
     const box1 = useRef()
     const box2 = useRef()
     const box3 = useRef()
+    const text =useRef()
 
 useEffect(()=>{
     gsap.from(box1.current,{
@@ -25,7 +26,8 @@ useEffect(()=>{
         scrollTrigger:{
             trigger:box2.current,
             markers: true,
-             scrub: 1, 
+            start: "top 50%",
+             
             
         }
     })
@@ -40,14 +42,31 @@ useEffect(()=>{
             
         }
     })
+    gsap.from(text.current,{
+        x:-500,
+        opacity:0,
+        duration:2,
+        scale:0,
+        delay:1,
+        scrollTrigger:{
+            trigger:text.current,
+            start: "top 50%", 
+            markers:true,
+              
+        }
+
+    })
 },[])
   return (
     <div>
         <div className='h-screen flex justify-center items-center bg-blue-300'>
             <div ref={box1} className=" w-50 h-50 bg-red-500 "></div>
         </div>
-        <div className='h-screen flex justify-center items-center bg-blue-500'>
-            <div ref={box2} className=" w-50 h-50 bg-red-600 "></div>
+        <div className='h-screen flex flex-col  justify-center items-center bg-blue-500 overflow-hidden'>
+                 <p ref={text} className='w-4/12 mx-auto text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta error repellat corrupti magnam ratione reiciendis, vero alias porro consequatur, at nesciunt facilis quasi! Quas, voluptatibus autem? Itaque in consectetur sequi.</p>
+            <div ref={box2} className=" w-50 h-50 bg-red-600 ">
+           
+            </div>
         </div>
         <div className='h-screen flex justify-center items-center bg-blue-900'>
             <div ref={box3} className=" w-50 h-50 bg-red-900 "></div>
